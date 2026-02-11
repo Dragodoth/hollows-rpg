4911b0ffff869363dce9ae6ac6e5abbbc632bb1f
Viktor <129966609+Dragodoth@users.noreply.github.com>
2026-02-11 15:52:10 +0100

    Update rollup.config.mjs

--- tools/rollup.config.mjs
+++ tools/rollup.config.mjs
@@ -1,14 +1,5 @@
 import css from "rollup-plugin-import-css";
 
-/**
- * Replaces all absolute asset paths with relative ones to preserve compatibility with route prefixing.
- * @param {string} code
- * @returns {string}
- */
-function replaceAbsolutePaths(code) {
-  return code.replaceAll("/systems/hollows-rpg/", "../");
-}
-
 export default [{
   input: "./hollows-rpg.mjs",
   output: {
@@ -23,9 +14,7 @@ export default [{
     format: "esm",
     assetFileNames: "hollows-rpg-system.css",
   },
-  plugins: [css({
-    transform: replaceAbsolutePaths,
-  })],
+  plugins: [css()],
 }, {
   input: "./code/styles/variables/_variables.mjs",
   output: {
@@ -33,9 +22,7 @@ export default [{
     format: "esm",
     assetFileNames: "hollows-rpg-variables.css",
   },
-  plugins: [css({
-    transform: replaceAbsolutePaths,
-  })],
+  plugins: [css()],
 }, {
   input: "./code/styles/elements/_elements.mjs",
   output: {
@@ -43,7 +30,5 @@ export default [{
     format: "esm",
     assetFileNames: "hollows-rpg-elements.css",
   },
-  plugins: [css({
-    transform: replaceAbsolutePaths,
-  })],
+  plugins: [css()],
 }];
