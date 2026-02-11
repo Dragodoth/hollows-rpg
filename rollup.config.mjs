005fa8e4c7d86a57f56e908689b54d6215388eae
Viktor <129966609+Dragodoth@users.noreply.github.com>
2026-02-11 15:56:04 +0100

    update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,18 +1,34 @@
-import postcss from "rollup-plugin-postcss";
-import resolve from "@rollup/plugin-node-resolve";
-import postcssImport from "postcss-import";
+import css from "rollup-plugin-import-css";
 
-export default {
-  input: "./_main.mjs",
+export default [{
+  input: "./hollows-rpg.mjs",
   output: {
-    file: "./public/main.mjs",
+    file: "./public/hollows-rpg.mjs",
     format: "esm",
+    sourcemap: true,
   },
-  plugins: [
-    resolve(),
-    postcss({
-      plugins: [postcssImport()],
-      extract: true,
-    }),
-  ],
-};
+}, {
+  input: "./code/styles/system/_system.mjs",
+  output: {
+    file: "./public/css/hollows-rpg-system.mjs",
+    format: "esm",
+    assetFileNames: "hollows-rpg-system.css",
+  },
+  plugins: [css()],
+}, {
+  input: "./code/styles/variables/_variables.mjs",
+  output: {
+    file: "./public/css/hollows-rpg-variables.mjs",
+    format: "esm",
+    assetFileNames: "hollows-rpg-variables.css",
+  },
+  plugins: [css()],
+}, {
+  input: "./code/styles/elements/_elements.mjs",
+  output: {
+    file: "./public/css/hollows-rpg-elements.mjs",
+    format: "esm",
+    assetFileNames: "hollows-rpg-elements.css",
+  },
+  plugins: [css()],
+}];
